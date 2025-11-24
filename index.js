e302641fb2d41b5bcd539df263145d892521510d
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-24 10:23:21 +0700

    menambahkan apiroute

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const experess = require('express');
 const connectDatabase = require('./config/db');
+const apiRoute = require('./routes/api');
 
 const app = experess(); 
 const PORT = 3000;
 
 app.use(experess.json());
 app.use(experess.urlencoded({ extended: false }));
+
+
+app.use('/api/v1', apiRoute);
 
 async function startServer() {
     await connectDatabase();
